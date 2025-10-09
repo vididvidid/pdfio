bdd1c68ba9bf42a9d15859c0469200baa0649292
vididvidid <vididvidid@gmail.com>
2025-10-09 11:50:29 +0000

    created separate testfile, updated the makefile, moved the pdf2cairo into separate folder

---
+++ testpdf2cairo.c
@@ -0,0 +1,125 @@
+//
+// Test Program for the pdf2cairo renderer.
+//
+
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#include "test.h"  // testBegin and testEnd functions come from here
+
+// Structure to hold a single renderer test case
+typedef struct
+{
+  const char *description; // A short description of the test
+  const char *input_file;  // Input PDF file name
+  const char *input_args;  // Command-line arguments for the input file
+  const char *output_option; // Output option: "o", "d", "T", or NULL
+  const char *output_filename; // Filename to be used with "-o", or NULL
+} renderer_test_t;
+
+// Common paths for test files
+static const char *input_path = "testfiles/renderer/";
+static const char *output_path = "testfiles/renderer-output/";
+
+// Array of renderer test cases
+static renderer_test_t tests[] = 
+{
+  { "Stroked box", "01_stroked_box.pdf", "-v -p 1 -r 300", "o", "01.png" },
+  { "Filled RGB box", "02_filled_box_rgb.pdf", "-v", "o", "02.png"}
+};
+
+// Main()
+int main(void)
+{
+  int i;
+  int status = 0;
+  int num_tests = sizeof(tests) / sizeof(tests[0]);
+
+  puts(" --- Running PDF2Cairo Renderer Tests --- ");
+
+  // Create the output directory
+  testBegin("Create renderer output directory");
+  if (system("mkdir -p testfiles/renderer-output") != 0)
+  {
+    testEndMessage(false, "Failed to create output directory.");
+    return (1);
+  }
+  testEnd (true);
+  
+  // Loop through all of the test cases
+  for (i = 0; i < num_tests; i++)
+  {
+    char command[2048]; // Command to execute
+
+    // Format the command that will be run
+    snprintf(command, sizeof(command), "./examples/pdf2cairo/pdf2cairo_main %s%s%s%s",
+        tests[i].input_args,
+        (tests[i].input_args[0]!='\0') ? " ": "",
+        input_path,
+        tests[i].input_file);
+
+    // Append output argumetns based on the test case specification
+    if (tests[i].output_option)
+    {
+      if (strcmp(tests[i].output_option, "o") == 0 && tests[i].output_filename)
+      {
+        // -o <path/filename>
+        strncat(command, " -o ", sizeof(command) - strlen(command) - 1);
+        strncat(command, output_path, sizeof(command) - strlen(command) - 1);
+        strncat(command, tests[i].output_filename, sizeof(command) - strlen(command) - 1);
+      }
+      else if (strcmp(tests[i].output_option, "d") == 0)
+      {
+        // -d <path>
+        strncat(command, " -d ", sizeof(command) - strlen(command) - 1);
+        strncat(command, output_path, sizeof(command) - strlen(command) - 1);
+      }
+      else if (strcmp(tests[i].output_option, "T") == 0)
+      {
+        // - T
+        strncat(command, " -T ", sizeof(command) - strlen(command) - 1);
+      }
+    }
+
+    // Run the test
+    testBegin(" Test: %s", tests[i].description);
+
+    if (system(command) != 0)
+    {
+      // Command failed; 
+      testEnd(false);
+      status = 1;
+    }
+    else 
+    {
+      // Command succeeded
+      testEnd(true);
+    }
+  }
+
+  puts(" --- Renderer Test finished. Check files in the testfiles/renderer-output/ --- ");
+
+  return (status);
+}
+
+    
+
+
+
+
+  
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
